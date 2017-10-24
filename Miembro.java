6297fcb3a471f9f1c5c3a8000cccc2b448ea8cc0
juanxo8 <32898064+juanxo8@users.noreply.github.com>
2017-10-24 21:25:30 +0200

    Revert "Commit 2"
    This reverts commit e99303047c45b00ecf3b2a6901a0ca364f4f46b4.

--- Miembro.java
+++ Miembro.java
@@ -7,28 +7,51 @@ public class Miembro
     private static int proximoSocio = 1;
     private int numSocio;
     private final String nombre;
+    private ArrayList<Moto> motos;             //motos que posee actualmente
     
     public Miembro(String nombre)
     {
+        motos = new ArrayList<Moto>();
         this.nombre = nombre;
         numSocio = proximoSocio;
         proximoSocio++;
     }
     
     @Override
-    public String toString()
-    {
+    public String toString(){
         String res = ("Miembro con numero de socio "+numSocio+" y nombre "+nombre);
         return res;
     }
-
-    public int getNumSocio() 
+    
+    public void setMoto(Moto moto)
     {
+        motos.add(moto);
+    }
+    
+    public ArrayList<Moto> getMotos(){
+        return motos;
+    }
+    
+    public int calcularCantidadMotos()
+    {
+        return motos.size();
+    }
+    
+    public float calcularPrecioMotos()
+    {
+        float precioTotal = 0;
+        for(int i = 0; i < motos.size(); i++)
+        {
+            precioTotal += motos.get(i).getCoste();
+        }
+        return precioTotal;
+    }
+
+    public int getNumSocio() {
         return numSocio;
     }
 
-    public String getNombre() 
-    {
+    public String getNombre() {
         return nombre;
     }
 }
